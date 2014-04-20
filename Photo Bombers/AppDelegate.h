1fa97dd1ffec37889b2f6ca6bbef62efddea84ac
Sebastian Doell <sebastian@pixelmilk.com>
2014-04-20 16:44:32 +0200

    Initial Commit

---
+++ Photo Bombers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photo Bombers
+//
+//  Created by Sebastian Döll on 20/04/14.
+//  Copyright (c) 2014 Pixelmilk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photo Bombers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photo Bombers
+//
+//  Created by Sebastian Döll on 20/04/14.
+//  Copyright (c) 2014 Pixelmilk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
